fd5ee3790ff18b22120c308c244fd88c693f4f21
oohlah <88854792+oohlah@users.noreply.github.com>
2025-12-12 08:20:22 +0000

    Feat: Added blynk.py
    - basic blynk code to read data from pin 1 & 2
    - inactivity feature - shutdown after 30 secs
    - connected wtih immediate temperature values

---
+++ blynk.py
@@ -0,0 +1,41 @@
+import BlynkLib, os
+from time import time, sleep
+from sense_hat import SenseHat
+
+sense = SenseHat()
+sense.clear()
+
+BLYNK_AUTH= os.getenv("BLYNK_AUTH")
+
+blynk = BlynkLib.Blynk(BLYNK_AUTH)
+
+#reading every 60 - timeout after 61 sec
+INACTIVITY_TIMEOUT = 30
+blynk.last_activity = time()
+
+#v1 is switch to turn off after inactivity
+@blynk.on("V1")
+def handle_v1_write(value):
+    button_value = value[0]
+    blynk.last_activity = time()  
+    print(f'Current button value: {button_value}')
+    if button_value=="1":
+        sense.clear(255,255,255)
+    else:
+        sense.clear()
+
+
+if __name__ == "__main__":
+    print("Blynk application started. Listening for events...")
+    try:
+        while True:
+            blynk.run()  # Process Blynk events
+            now = time()
+            blynk.virtual_write(0,sense.temperature) 
+            #exit if no activity
+            if now - blynk.last_activity > INACTIVITY_TIMEOUT:
+                print(f"No activity for {INACTIVITY_TIMEOUT} seconds. Exiting.")
+                break
+        sleep(2)  # delay for CPU usage
+    except KeyboardInterrupt:
+        print("Blynk application stopped.")
