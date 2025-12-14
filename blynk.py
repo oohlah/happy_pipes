e5a2d369549deb09156ca713dc9e0a019c6bbcfc
oohlah <88854792+oohlah@users.noreply.github.com>
2025-12-14 09:41:44 +0000

    feat: url sent via http api to Blynk

--- blynk.py
+++ blynk.py
@@ -2,6 +2,10 @@ import BlynkLib, os
 from time import time, sleep
 from sense_hat import SenseHat
 
+from upload_cloudinary import image_url
+
+#import requests library for url encoding
+import requests
 
 sense = SenseHat()
 sense.clear()
@@ -27,16 +31,40 @@ def handle_v1_write(value):
         sense.clear()
 
 
+image_sent = False
+
+def send_image():
+     global image_sent
+     temp = sense.get_temperature()
+     if temp < 26.5 and not image_sent:
+        image = image_url() #store image in url variable
+        print(f"Sending image URL to Blynk: {image}")
+        
+        url = f"https://blynk.cloud/external/api/update?token={BLYNK_AUTH}&pin=V2&value={image}"
+
+        
+        encoded_data = requests.models.RequestEncodingMixin._encode_params(url)
+        print(encoded_data)
+       
+        try:
+            response = requests.get(encoded_data)
+            image_sent = True
+        except Exception as e:
+            print(f"Failed to send image URL")
+        
+
 if __name__ == "__main__":
     print("Blynk application started. Listening for events...")
     try:
         while True:
             blynk.run() #run Blynk
+            image = image_url() #store image in url variable
             temp = sense.get_temperature() #get temperature from sense_hat
             blynk.virtual_write(0, temp) #write temperature
             print(f"Temperature: {temp}°C")
             if temp < 26.5:
                 blynk.log_event("warning_temp_event")
+                send_image()
 
             now = time()
             if now - blynk.last_activity > INACTIVITY_TIMEOUT:
--- upload_cloudinary.py
+++ upload_cloudinary.py
@@ -15,11 +15,11 @@ STATIC_DIR = os.path.join(BASE_DIR, "static")
 IMAGE_PATH = os.path.join(STATIC_DIR, "last_env_image.jpg")
 
 #upload image and return public url - allow overwrite
-def upload_image(image_path,
+def upload_image(image_path = IMAGE_PATH, #explicitly setting image_path to IMAGE_PATH so it can be called without arguments
                  folder = "environment_camera",
                  public_id = "last_env_image"):
     result = cloudinary.uploader.upload(
-        image_path,
+        image_path, 
         folder="environment_camera",
         public_id="last_env_image",
         overwrite = True,
@@ -28,10 +28,17 @@ def upload_image(image_path,
     url = result["secure_url"]
     return url
 
+
+def image_url():
+    return upload_image(IMAGE_PATH)
+
 def main():
     url = upload_image(IMAGE_PATH)
     print("Upload Complete")
     print(f"Public URL: {url}")
+    print (image_url())
+
+
 
 if __name__ == "__main__":
     main()
