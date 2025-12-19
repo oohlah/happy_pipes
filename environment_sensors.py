eb352e6ed855d69815ebeaae6b5c3be5b5b082ac
oohlah <88854792+oohlah@users.noreply.github.com>
2025-12-19 07:17:05 +0000

    feat: modified environment_sensors to only handle LED data from SenseHat

--- environment_sensors.py
+++ environment_sensors.py
@@ -1,27 +1,7 @@
 from sense_hat import SenseHat
-#math library
-import math
 
 sense = SenseHat()
 
-#function to get dewpoint from temp and humidity
-def get_dew_point(temp, humidity):
-    A = 17.27
-    B = 237.7
-    alpha = ((A * temp) / (B + temp)) + math.log(humidity/100.0)
-    return (B * alpha) / (A - alpha)
-
-#funcion returns environemnt reading in python dictionary
-def get_env_stats():
-    temp = round(sense.get_temperature(),)
-    humidity = round(sense.get_humidity(), 2)
-    dew = round(get_dew_point(temp, humidity), 2)
-
-    return { 
-        "temperature_c": temp,
-        "humidity": humidity,
-        "dew_point": dew
-    }
 
 #LED RED
 def led_red():
