5f597bdb83c093c0bc0516bed25acc318d112c7b
oohlah <88854792+oohlah@users.noreply.github.com>
2025-12-21 11:05:45 +0000

    feat: modified environment_camera to act as a utils for camera functions

--- environment_camera.py
+++ environment_camera.py
@@ -3,11 +3,7 @@
 import time, os
 from datetime import datetime
 
-import json
-
 from picamera2 import Picamera2
-
-from upload_cloudinary import upload_image
 
 
 #use os to set up base and static folder 
@@ -22,78 +18,28 @@ os.makedirs(STATIC_DIR, exist_ok=True)
 #set image path - to add image to static_dir folder
 IMAGE_PATH = os.path.join(STATIC_DIR, "last_env_image.jpg")
 
+picam2=None #camera doesn't start on boot
 
-picam2 = Picamera2()
-picam2.configure(picam2.create_still_configuration(main={"size": (3280,2464)}))
-picam2.start()
-print("Camera started. Image will be taken once temperature drops")
 STATE_DIR = os.path.join(BASE_DIR, "state")
 os.makedirs(STATE_DIR, exist_ok=True)
 STATE_PATH = os.path.join(STATE_DIR, "environment.json")
 
-#read json file with saved env_state
-def read_env_state():
-   try:
-       with open(STATE_PATH, "r") as f:
-           return json.load(f)
-   except Exception:
-       return None
-   
+# initialise camera, capture photo and save to image_path when called
 def capture_photo():
-    print("Capturing Environment Image")
-    picam2.capture_file(IMAGE_PATH) #capture file function stores image to path in argument
-    time.sleep(0.3)
-    print("Image Saved to: ", IMAGE_PATH)
-
-#save new env_state with url of image once taken
-def save_state(image_url=None):
-   env = read_env_state() or {}
-   payload = env.copy()
-
-   #update image if it is not empty
-   if image_url is not None:
-        payload["image"] = image_url #not saved
-        # Add/Update image info and timestamps
-        payload["ts"] = int(datetime.now().timestamp())
-        payload["iso"] = datetime.now().isoformat(timespec="seconds")
-
-
-   try:
-       with open(STATE_PATH, "w") as f:
-           json.dump(payload, f, indent=2)
-       print("State saved:", payload)
-   except Exception as e:
-       print("Error saving state:", e)
-    
-    
-try:
-    while True:
-            #read current json file for env_state
-            env = read_env_state()
-            if env is None or "temperature_c" not in env:
-                time.sleep(1)
-                continue
-
-
-            temp = env["temperature_c"]
-            print("Temperature:", temp)
-
-
-
-            #if temp less than hardcoded for now
-            if temp <= 0:
-                capture_photo()
-                image_url = upload_image(IMAGE_PATH)
-                save_state(image_url)  # Save env stats + image URL
-                time.sleep(10)  # Wait short time to test camera
-
-
+        try:
+            picam2 = Picamera2()
+            picam2.configure(picam2.create_still_configuration(main={"size": (3280,2464)}))
+            picam2.start()
+            print("Camera started. Image will be taken once temperature drops")
+            print("Capturing Environment Image")
+            picam2.capture_file(IMAGE_PATH) 
+            time.sleep(0.3)
+            print("Image Saved to: ", IMAGE_PATH)
+            picam2.stop()
+            return IMAGE_PATH
+        except Exception as e:  # catch any error
+            print(f"Camera error: {e}")
             time.sleep(1)
-
-
-except KeyboardInterrupt:
-    print("Exiting")
-finally:
-    picam2.stop()
+        raise Exception("Failed to capture image")
 
 
