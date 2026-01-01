7932de346d2fe30e1614d1c8f9d07903fd7fe5b7
oohlah <88854792+oohlah@users.noreply.github.com>
2026-01-01 11:59:32 +0000

    fix: fixed indent on if statement - outside of loop

--- chart.py
+++ chart.py
@@ -32,37 +32,47 @@ def generate_chart():
         lines = csv.reader(csvfile, delimiter=',')
         next(lines)  # skip header
         for row in lines:
+            print(f"reading row?: {row}")
+
             if row[0] and row[2] and row[5]:
-                x.append(datetime.fromisoformat(row[5]))        # ISO timestamp column on x-axis, 5 index
-                y_temperature.append(float(row[0]))            # temperature on y-aaxis, 0 index in csv
-                y_dew_point.append(float(row[2]))               # dew_point - y, 2nd index in csv
+                try:
+                    x.append(datetime.fromisoformat(row[5]))        # ISO timestamp column on x-axis, 5 index
+                    y_temperature.append(float(row[0]))            # temperature on y-aaxis, 0 index in csv
+                    y_dew_point.append(float(row[2]))               # dew_point - y, 2nd index in csv
+                except Exception as e:
+                    print(f"error {e} has occured. skip row")
 
+    if not x:
+            print("no valid data to plit")
+            return
+            
         # non-GUI backend so Matplotlib can be used in flask
-        plt.switch_backend('Agg')
-        # create the figure
-        plt.figure(figsize=(10,5))
+    plt.switch_backend('Agg')
+    # create the figure
+    plt.figure(figsize=(10,5))
 
-        # plot the temperature data
-        plt.plot(x, y_temperature, color='r', linestyle='-', marker=None,
-             label="Temperature °C", linewidth=1)
-        plt.plot(x, y_dew_point, color='b', linestyle='-', marker=None,
-             label="Dew Point °C", linewidth=1)
+    # plot the temperature data
+    plt.plot(x, y_temperature, color='r', linestyle='-', marker=None,
+            label="Temperature °C", linewidth=1)
+    plt.plot(x, y_dew_point, color='b', linestyle='-', marker=None,
+            label="Dew Point °C", linewidth=1)
 
-        # format x-axis labels
-        plt.xticks(rotation=25)
-        plt.xlabel('Time')
-        plt.ylabel('Value')
-        plt.title('Temperature & Dew Point Over Time', fontsize=16)
-        plt.grid(True)
-        plt.legend()
-        plt.tight_layout()
+    # format x-axis labels
+    plt.xticks(rotation=25)
+    plt.xlabel('Time')
+    plt.ylabel('Value')
+    plt.title('Temperature & Dew Point Over Time', fontsize=16)
+    plt.grid(True)
+    plt.legend()
+    plt.tight_layout()
 
-        # save the chart as a PNG in static folder
-        plt.savefig(CHART_PATH)
-        plt.close()
-        print(f"Chart saved to {CHART_PATH}")
+    # save the chart as a PNG in static folder
+    plt.savefig(CHART_PATH)
+    plt.close()
+    print(f"Chart saved to {CHART_PATH}")
 
 
-       
-
-   
+ #test      
+# if __name__ == "__main__":
+#    print(f"Running chart.py")
+#    generate_chart()
